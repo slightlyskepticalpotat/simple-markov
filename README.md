fa524d17440d41d171fcf32d3c0722539b692734
cg <31290848+slightlyskepticalpotat@users.noreply.github.com>
2021-02-24 23:51:00 -0500

    Actual program

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # simple-markov
-A simple and fast Markov Chain-based text generator in C++.
+A simple and fast [Markov Chain](https://en.wikipedia.org/wiki/Markov_chain)-based text generator in C++. It reads space-delimited tokens from a text file, and can be used to generate individual words or letters in sequence. The number of previous states used, number of words to generate, and temperature (how "random" the generated text is) are all configurable. A text file of Shakespeare's plays and sonnets has been included for convenience. Here is some annotated example code:
+
+```c++
+int main() {
+    ios_base::sync_with_stdio(false); // makes the i/o faster
+    cin.tie(NULL); // also makes the i/o faster
+    Markov chain("shakespeare.txt", 3); // name of the text file, previous states used
+    chain.generateText(128, 2); // number of words to generate, temperature from 0-100
+    return 0;
+}
+```
---
+++ markov.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Markov {
+    public:
+        map<deque<string>,  vector<string>> state;
+        vector<string> all_tokens;
+        int state_length;
+        Markov(string input_text, int n) {
+            ifstream file(input_text);
+            deque<string> current;
+            string buff;
+            state_length = n;
+            srand(time(0));
+            for (int i = 0; i < state_length; i++) { // placeholder of newlines
+                addWord(current, "\n");
+            }
+            while (file >> buff) { // add the main body of the text
+                addWord(current, buff);
+                all_tokens.push_back(buff);
+            }
+            addWord(current, "\n"); // mark the end of the text
+            file.close();
+        }
+        
+        void generateText(int to_generate, int temperature) {
+            deque<string> current;
+            string buff;
+            int n = all_tokens.size();
+            int i = 0;
+            for (int i = 0; i < state_length; i++) { // placeholder of newlines
+                addWord(current, "\n");
+            }
+            while (i < to_generate) {
+                vector<string>& next = state[current];
+                if ((rand() % 100 + 1) <= temperature) { // introduce a little anarchy
+                    cout << all_tokens[rand() % n] << " ";
+                    i += 1;
+                    continue;
+                }
+                string buff = next[rand() % next.size()];
+                if (buff != "\n") {
+                    cout << buff << " ";
+                    current.pop_front();
+                    current.push_back(buff);
+                    i += 1;
+                }
+            }
+            cout << "\n";
+        }
+    private:
+        void addWord(deque<string>& current, string extend) {
+            if ((int) current.size() == state_length) {
+                state[current].push_back(extend);
+                current.pop_front();
+            }
+            current.push_back(extend);
+        }
+};
